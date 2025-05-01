4a4f61a0b7162358f7062ac75dfbc2b799e27274
Naveen kumar vemundla <naveenkumar15261@gmail.com>
2025-05-02 01:11:58 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull naveen1526/simple-python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 naveen1526/simple-python-app
+docker run -d -p 8000:8000 naveen1526/simple-python-app
